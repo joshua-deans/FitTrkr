e243332772e2d384a07e7ef875ab62dc8cec23f4
Giovanni HoSang <giovanni.hosang@gmail.com>
2018-07-22 17:09:23 -0700

    Setting up MYSQLDB with python

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, url_for, request, abort, redirect
+from flask_mysqldb import MySQL
+from db import DBconfig
+
 
 app = Flask(__name__)
+
+DBconfig = DBconfig()
+
+# COnfigure DB
+app.config['MYSQL_HOST'] = DBconfig["host"]
+app.config['MYSQL_USER'] = DBconfig["user"]
+app.config['MYSQL_PASSWORD'] = DBconfig["password"]
+app.config['MYSQL_DB'] = DBconfig["DBName"]
+app.config['MYSQL_CURSORCLASS'] = DBconfig["dictDB"]
+
 
 # Route for landing page
 @app.route("/")
