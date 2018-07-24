a6e5b84ddb7138facc48ca55b787af829e2e5719
jdeans <joshua.deans21@gmail.com>
2018-07-23 21:09:31 -0700

    Browse Plans table now is populated from the database.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from os import urandom
 import scrypt
 import base64
 
-
 app = Flask(__name__)
 
 DBconfig = DBconfig()
@@ -21,6 +20,7 @@ app.config['MYSQL_CURSORCLASS'] = DBconfig["dictDB"]
 # init MYSQL
 mysql = MySQL(app)
 
+
 def is_logged_in(flask_request: Flask.request_class) -> (bool, int):
     cookies = flask_request.cookies
     if 'token' in cookies:
@@ -34,11 +34,13 @@ def is_logged_in(flask_request: Flask.request_class) -> (bool, int):
             return True, result['UserID']
     return False, -1
 
+
 # Route for landing page
 @app.route("/")
 def base():
     logged_in_as = is_logged_in(request)
     return render_template('base.html')
+
 
 class SignupForm(Form):
     username = StringField('Username', [
@@ -50,9 +52,10 @@ class SignupForm(Form):
     confirm = PasswordField('Confirm password', [
         validators.DataRequired()])
 
+
 # Route for sign up form
 
-@app.route("/signup", methods=['GET', 'POST'])
+@app.route("/signup/", methods=['GET', 'POST'])
 def signup():
     form = SignupForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -70,6 +73,7 @@ def signup():
         return redirect(url_for('login'))
     return render_template('auth/signup.html', form=form)
 
+
 class LoginForm(Form):
     username = StringField('Username', [
         validators.DataRequired(),
@@ -77,8 +81,9 @@ class LoginForm(Form):
     password = PasswordField('Password', [
         validators.DataRequired()])
 
+
 # Route for sign up form
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login/", methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -112,23 +117,32 @@ def login():
 
 
 ### Delete before merging
-@app.route("/client")
-def client():
+@app.route("/client/<int:user_id>/")
+def client(user_id):
     return render_template('client/dashboard.html')
 
 
-@app.route("/trainer")
-def trainer():
+@app.route("/client/<int:user_id>/plans/")
+def client_browse_plans(user_id, plan_info=None):
+    cur = mysql.connection.cursor()
+    cur.execute(
+        'SELECT f.FitnessProgramID, u.FirstName, u.LastName, f.FP_intensity, f.Description, f.Program_Length, '
+        'f.MealPlanID, f.WorkoutPlanID '
+        'FROM FitnessProgram f, Users u WHERE f.TrainerID = u.UserID')
+    result = cur.fetchall()
+    if result:
+        plan_info = result
+    cur.close()
+    return render_template('client/browse_plans.html', plan_info=plan_info)
+
+
+@app.route("/trainer/<int:user_id>/plans")
+def trainer(user_id):
     return render_template('trainer/dashboard.html')
 
-
-@app.route("/client/plans")
-def client_browse_plans():
-    return render_template('client/browse_plans.html')
 
 ### Delete before merging
 
 # Note: This is in debug mode. This means that it restarts with changes
 if __name__ == "__main__":
     app.run(debug=True)
-
